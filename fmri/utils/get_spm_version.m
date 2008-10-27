5d8ac1b8c54f4e891e21f44a9471912ab8f41843
pjanata <pjanata@ucdavis.edu>
2008-10-27 06:05:41 +0000

    SPM compatibility stuff
    git-svn-id: https://atonal.ucdavis.edu/repos/public/matlab@36 43baf2fa-511e-4498-8ce5-83910217ac2c

--- fmri/utils/get_spm_version.m
+++ fmri/utils/get_spm_version.m
@@ -2,7 +2,14 @@ function version_num = get_spm_version()
 % Checks to see which version of SPM5 we are dealing with. This is a bit 
 % tenuous because we are assuming version.txt for SPM5 is on path before
 % any other possible instance of version.txt
+%
+% Assume version# 958 if version.txt not found. This is really dicey.
 
-version_info = textread('version.txt','%s');
-revidx = strmatch('Revision:', version_info);
-version_num = str2num(version_info{revidx+1});
+
+if ~exist('version.txt')
+  version_num = 957;
+else
+  version_info = textread('version.txt','%s');
+  revidx = strmatch('Revision:', version_info);
+  version_num = str2num(version_info{revidx+1});
+end
