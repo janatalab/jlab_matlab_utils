a425b366f6f15c256969ac454c99537150d3f28c
fbarrett <fsbarrett@ucdavis.edu>
2009-02-24 19:59:26 +0000

    adding matlab/utils/compare_cells.m, updating compare_structs.m to use this
    git-svn-id: https://atonal.ucdavis.edu/repos/public/matlab@86 43baf2fa-511e-4498-8ce5-83910217ac2c

---
+++ utils/compare_cells.m
@@ -0,0 +1,180 @@
+function [cellsAreEqual,firstViolation,violationReason] = compare_cells(cell1,cell2)
+
+% this function compares the structure and contents of multi-dimensional and nested cells
+% 
+% this function has been modeled after compare_structs.m (S. Tomic)
+% 
+% cell1 and cell2 are the only required arguments
+% 
+% the rest of the arguments are maintained for compatibility with
+% compar_struct, and are passed onto compare_struct in the event that cell
+% contents are of class 'struct'. tag,value pairs that specify how the
+% structs will be compared.
+% supported tags and values:
+% 
+%    'values' (true or false): specifies whether field values will be
+%       compared or not.
+%    'substruct' (true or false): specifies whether to check only whether
+%       struct1 is contained in struct2 (struct2 simply has more fields and
+%       values than struct1 but otherwise are equal). Otherwise the structs
+%       will be compared for an exact match.
+%    'types' (true or false): specifies whether or not to check the type
+%       (class) of the fields for a positive match. This can only be set to
+%       false if 'values' is also false. Otherwise, types will
+%       automatically be checked.
+% 
+% this function first checks to see if both inputs are cells, and then
+% compares the structure (# dimensions and size in each dimension) of the
+% provided cells. Any inequality in # of dimensions or size in any
+% dimension will be viewed as a cell inequality. If cells are of identical
+% structure, they will be converted to a vector [new_vect = cellN(:)], and
+% then each element from both cells will be compared on variable class and
+% then content.
+% 
+%  OUTPUTS
+%   cellsAreEqual - 1 or 0, whether or not cell equality is true
+%   firstViolation - the index in the vector-transformed cells where the
+%       first inequality of content or variable class was found. In the
+%       case of cells within cells, this function is called recursively.
+%       when recursing on self, upon finding an error, the index in the
+%       topmost parent cell where the inequality was found is returned in
+%       firstViolation. In the case that the structure of the cells
+%       differs, or the case that either or both of the inputs are not
+%       cells, firstViolation = 0;
+%   violationReason - provides a concise descriptor of the type of
+%       violation that occurred.
+% 
+% NOTE: if one cell is a transposition of the other, they will be found
+% unequal, either by size, or by contents
+% 
+% FB 209.02.24
+
+% 
+% initialize variables
+% 
+cellsAreEqual = 0;
+firstViolation = 0;
+violationReason = '';
+
+numberTypes = {'int8','uint8','int16','uint16','int32','uint32','int64',...
+    'uint64','double'};
+
+if (nargin > 2)
+  for iarg = 1:nargin-2
+    switch(varargin{iarg})
+     case 'values'
+      checkValues = varargin{iarg+1};
+     case 'substruct'
+      checkSubstruct = varargin{iarg+1};
+     case 'types'
+      checkTypes = varargin{iarg+1};
+    end
+  end
+end
+ 
+try
+  checkValues;
+catch
+  checkValues = true;
+end
+try
+  checkSubstruct;
+catch
+  checkSubstruct=false;
+end
+try
+  checkTypes;
+catch
+  checkTypes=true;
+end
+
+% 
+% check to see if inputs are cells
+% 
+if ~iscell(cell1) && ~iscell(cell2)
+  violationReason = 'neither input is of type ''cell''';
+  return
+elseif ~iscell(cell1)
+  violationReason = 'cell1 is not of type ''cell''';
+  return
+elseif ~iscell(cell2)
+  violationReason = 'cell2 is not of type ''cell''';
+  return
+end
+
+% 
+% compare sizes of cells
+% 
+s1 = size(cell1);
+s2 = size(cell2);
+
+% if cells are not the same exact size, they will be treated as unequal
+% NOTE: transposed cell arrays will not be treated as equal
+if length(s1) ~= length(s2)
+  violationReason = 'cells have different number of dimensions';
+  return
+end
+
+sm = (s1 == s2);
+if ~all(sm)
+  violationReason = 'cells have different sizes in one or more dimension';
+end
+
+% convert cells to vectors
+v1 = cell1(:);
+v2 = cell2(:);
+nv = length(v1);
+
+% step through cells, compare at each index
+for i=1:nv
+    
+  % compare cell class
+  c1 = class(v1{i});
+  c2 = class(v2{i});  
+  if isempty(strmatch(c1,c2,'exact'))
+    firstViolation = i;
+    violationReason = sprintf(['cell contents have different class at '...
+        'index %d'],i);
+    return
+  end
+
+  % compare cell contents
+  same = 1;
+  switch c1     
+      case numberTypes
+        if any(size(v1{i}) ~= size(v2{i})) || ~all(v1{i} == v2{i})
+          same = 0;
+          subVioReason = sprintf('%s differed',c1);
+        end
+      case 'char'
+        if ~strcmp(v1{i},v2{i})
+          same = 0;
+          subVioReason = 'strings differed';
+        end
+      case 'struct'
+        [structsAreEqual,subStructVio,subVioReason] = compare_structs(...
+            v1{i},v2{i},'values',checkValues,'substruct',checkSubstruct,...
+            'types',checkTypes);
+        if ~structsAreEqual, same = 0; end
+      case 'cell'
+        [subCellsEqual,subCellVio,subVioReason] = compare_cells(v1{i},v2{i});
+        if ~subCellsEqual, same = 0; end
+      case 'logical'
+        if v1{i} ~= v2{i}
+          same = 0;
+          subVioReason = sprintf('logical vars differed');
+        end          
+      otherwise
+        same = 0;
+        subVioReason = sprintf('class %s not supported',c1);
+  end
+
+  if ~same
+    firstViolation = i;
+    violationReason = sprintf('cell contents differed (%s)',subVioReason);
+    return
+  end
+end % for i=1:nv
+
+% no differences found, return equality
+cellsAreEqual = 1;
--- utils/compare_structs.m
+++ utils/compare_structs.m
@@ -41,7 +41,7 @@ function [structsAreEqual,firstViolation,violationReason] = compare_structs(stru
 % 2007 - Stefan Tomic, first version
 % 10/8/2008 - Stefan Tomic, extracted from check_anal_exist and added tag,value functionality
 % 12/30/2008 - Fred Barrett, added support for multi-dimensional cell arrays as field values
-%
+% 02/24/2009 - Fred Barrett, switched cell checking to compare_cells.m
 
 numberTypes = {'int8','uint8','int16','uint16','int32','uint32','int64','uint64','double'};
 firstViolation = {};
@@ -153,49 +153,14 @@ for iField = 1:struct1_nFields
     fieldsAreEqual = strcmp(struct1_val,struct2_val);
   
    case 'struct'
-    [fieldsAreEqual,subFieldViolation,violationReason] = compare_structs(struct1_val,struct2_val,'values',checkValues,'substruct',checkSubstruct,'types',checkTypes);
+    [fieldsAreEqual,subFieldViolation,violationReason] = ...
+        compare_structs(struct1_val,struct2_val,'values',checkValues,...
+        'substruct',checkSubstruct,'types',checkTypes);
  
    case 'cell'
-    % compare cell sizes
-    sa = size(struct1_val);
-    sb = size(struct2_val);
+    [fieldsAreEqual,subFieldViolation,violationReason] = ...
+        compare_cells(struct1_val,struct2_val);
     
-    % if cells are not the same exact size, they will be treated as unequal
-    % note, transposed cell arrays will not be treated as equal
-    try sm = (sa == sb);
-    catch
-     fieldsAreEqual = 0;
-    end
-    if ~all(sm)
-     fieldsAreEqual = 0;
-    end
-
-    if(length(sa) > 2)
-     error(['struct1_val is more than 2 dimensions, this size is '...
-         'not yet supported']);
-    end
-
-    if (all(sa > 1))
-      warning('multi-dimensional array, pretending structs match');
-      fieldsAreEqual = 1;
-    elseif ~any([iscellstr(struct1_val),iscellstr(struct2_val),...
-            ischar(struct1_val),ischar(struct2_val)])
-      warning('nested structs, pretending they match');
-      fieldsAreEqual = 1;
-    else
-        %only cell array of strings is supported
-        [c,ia,ib] = intersect(struct1_val,struct2_val);
-
-        %make sure all elements are in common and in the same order
-        if(length(c) == length(struct1_val) ...
-           && length(c) == length(struct2_val) ...
-           && all(ia == ib))
-          fieldsAreEqual = 1;
-        else
-          fieldsAreEqual = 0;
-        end
-    end
-
    case 'logical'
     if struct1_val == struct2_val
      fieldsAreEqual = 1;
