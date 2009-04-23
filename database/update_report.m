9b4b342bb743ceb7872e266a10fb1fd11f0da23f
fbarrett <fsbarrett@ucdavis.edu>
2009-04-23 00:43:14 +0000

    adding update_report.m
    git-svn-id: https://atonal.ucdavis.edu/repos/public/matlab@125 43baf2fa-511e-4498-8ce5-83910217ac2c

---
+++ database/update_report.m
@@ -0,0 +1,16 @@
+function r = update_report(r, report_str);
+%
+
+if ~isfield(r,'msglog')
+  r.msglog = {};
+end
+
+r.msglog = [r.msglog;{report_str}];
+
+if r.report_on_fly
+  fprintf('%s',report_str)
+end
+
+if isfield(r,'logfile_fid')
+  fprintf(r.logfile_fid,'%s',report_str);
+end
