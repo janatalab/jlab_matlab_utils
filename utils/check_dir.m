e88dad41733b09d246406060c48cd015bdd49982
fbarrett <fsbarrett@ucdavis.edu>
2009-08-19 19:04:46 +0000

    adding flag to check_dir to create parent dirs as needed
    git-svn-id: https://atonal.ucdavis.edu/repos/public/matlab@220 43baf2fa-511e-4498-8ce5-83910217ac2c

--- utils/check_dir.m
+++ utils/check_dir.m
@@ -1,7 +1,7 @@
-function status = check_dir(outdir, verbose)
+function status = check_dir(outdir, verbose, parents)
 % Checks for existence of outdir, and creates it if necessary
 %
-% status = check_dir(outdir, verbose);
+% status = check_dir(outdir, verbose, recurse);
 %
 % status returns 0 on success and 1 on failure
 %
@@ -9,15 +9,25 @@ function status = check_dir(outdir, verbose)
 % outdir:  a string that specifies the directory to check.
 % verbose: 0 or 1. 1 means that some basic status messages will
 %          output to the command line
+% parents: 0 or 1. 1 means that check_dir will execute mkdir with the '-p'
+%          argument, if outdir does not exist. This will create parent
+%          directories as needed. execute 'man mkdir' from the unix command
+%          line for more information
 %
 % Copyright (c) 2005 The Regents of the University of California
 % All Rights Reserved
 %
 % Author(s):
 % 10/18/05 Petr Janata
+% 08/17/09 Fred Barrett - added recursive option
 
 if ~exist('verbose','var')
   verbose = 0; 
+end
+if exist('parents','var') && parents
+  mkdiropts = '-p ';
+else
+  mkdiropts = '';  
 end
 status = -1;
 
@@ -28,7 +38,7 @@ if exist(outdir) ~= 7
   %escape spaces and other strange characters for mkdir
   outdir = regexprep(outdir,'[()'',& ]','\\$0');
     
-  unix_str = sprintf('mkdir %s', outdir);
+  unix_str = sprintf('mkdir %s%s',mkdiropts,outdir);
   status = unix(unix_str);
 
 else
