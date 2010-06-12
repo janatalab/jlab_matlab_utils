5ec84edc998b7e9154a498952b6bab13897d998a
pjanata <pjanata@ucdavis.edu>
2010-06-12 21:54:49 +0000

    minor changes
    git-svn-id: https://atonal.ucdavis.edu/repos/public/matlab@448 43baf2fa-511e-4498-8ce5-83910217ac2c

--- utils/prob2str.m
+++ utils/prob2str.m
@@ -1,19 +1,27 @@
-function [str] = prob2str(prob,crit,tok)
-% [str] = prob2str(prob,crit,tok)
+function [str] = prob2str(prob,crit,tok,places)
+% [str] = prob2str(prob,crit,tok,places)
 %
 % Converts probability value in prob to a sequence of tokens.
+%
+% crit - criterion probability above which to return 'n.s.'
+% tok - token to use for string. Default: *
+% places - number of places to go out to. Default: 1E-4 = ****
 %
 % E.g. 0.001 -> ***
 %
 
 % 03/05/05/ PJ
 
+if nargin < 4
+  places = 4;
+end
+
 if nargin < 3
   tok = '*';
 end
 
-if prob < 1E-5
-  str = repmat(tok,1,5);
+if prob < 10^-places
+  str = repmat(tok,1,places);
 elseif prob <= crit
   str = repmat(tok,1,abs(fix(log10(prob))));
 else
