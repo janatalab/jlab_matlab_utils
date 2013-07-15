4d5a868da1787ac843990f04b7909d3c7f4f95f0
pjanata <pjanata@ucdavis.edu>
2013-07-15 20:48:13 +0000

    formatting cleanup of ensemble_jobman
    git-svn-id: https://atonal.ucdavis.edu/repos/public/matlab@702 43baf2fa-511e-4498-8ce5-83910217ac2c

--- database/ensemble_jobman.m
+++ database/ensemble_jobman.m
@@ -1,9 +1,9 @@
 function analysis_list = ensemble_jobman(analysis_list,params)
 % Top-level job manager for the Ensemble Matlab analysis toolkit.
-% 
+%
 % analysis_list = ensemble_jobman(analysis_list,params)
 %
-% analysis_list is a cell array of analysis structures that 
+% analysis_list is a cell array of analysis structures that
 % conform to the ensemble analysis structure spec (see wiki for details).
 % possible params are:
 %
@@ -34,7 +34,7 @@ function analysis_list = ensemble_jobman(analysis_list,params)
 % 06/15/10 - PJ cleaning mysql_make_conn support
 % 04/11/11 - PJ added alternative loading of results struct from file
 % 10Nov2012 - PJ added and fixed insertion of analysis name into the
-%             results structure 
+%             results structure
 
 
 %if conn_id is set in params, then open a mysql connection
@@ -42,10 +42,10 @@ function analysis_list = ensemble_jobman(analysis_list,params)
 %specify the same conn_id in their params
 conn_local = false;
 
-try 
-  conn_id = params.ensemble.conn_id; 
+try
+  conn_id = params.ensemble.conn_id;
 catch
-  conn_id = []; 
+  conn_id = [];
   conn_local = false;
 end
 
@@ -84,64 +84,64 @@ for ia = idxs
       anSearchCrit.name = requiredData.name;
       anListIdx = ensemble_find_analysis_struct(analysis_list,anSearchCrit);
       if(isempty(anListIdx))
-				error(sprintf('Analysis %s specifies a non-existent required analysis %s',...
-					analysis_list{ia}.name,anSearchCrit.name));
-			end
-	
-			numIdx = length(anListIdx);
-			if numIdx > 1
-				error('Found %d analysis structs (%s) that matched the name of the required structure: %s', ...
-					numIdx, sprintf('%d ', anListIdx), anSearchCrit.name)
-			end
-			
-			if(isfield(analysis_list{anListIdx},'results') )
-	%search for the required data type
-
-	dataList = analysis_list{anListIdx}.results;
-  
-  % Handle the possibility that the results is simply a flag and that the
-  % results are stored to disk, e.g. if the analysis was run using
-  % ensemble_jobman_parallel.
-  if isnumeric(dataList) && dataList == 1 && ...
-      isfield(analysis_list{anListIdx},'path') && ~isempty(analysis_list{anListIdx}.path)
-    tmp = load(analysis_list{anListIdx}.path);
-    dataList = tmp.job_data.results;
-  end
-    
-	%see if 'vars' was specified, in which case we need to
-        %search for the variable to return
-	if(isfield(requiredData,'vars') && ~isempty(requiredData.vars))
-
-    
-	  %find the index of the result we need
-	  dataListIdx = strmatch(requiredData.vars,dataList.vars);
-	  
-	  %if result data struct was found then assign to indata
-	  %otherwise, throw an error indicating the data is not there.
-	  if(~isempty(dataListIdx))
-	    indata{requiredIdx} = dataList.data{dataListIdx};
-	  
-	  else
-	    error([sprintf('Analysis %s specifies a non-existent required',analysis_name)...
-		   sprintf(' datatype %s for analysis %s',requiredData.vars,analysis_list{anListIdx}.name)]);
-	  end
-	
-	else
-	  %if the 'vars' field of the required data list was not
-          %specified, then return the whole results data structure
-	  indata{requiredIdx} = analysis_list{anListIdx}.results;
-	end
-	
-	  
-      else
-	
-	%results field wasn't populated so required data wasn't available
-	error(sprintf('Required data for analysis ''%s'' was not available.',analysis_name));
-      
+        error(sprintf('Analysis %s specifies a non-existent required analysis %s',...
+          analysis_list{ia}.name,anSearchCrit.name));
       end
       
-    end
-       
+      numIdx = length(anListIdx);
+      if numIdx > 1
+        error('Found %d analysis structs (%s) that matched the name of the required structure: %s', ...
+          numIdx, sprintf('%d ', anListIdx), anSearchCrit.name)
+      end
+      
+      if(isfield(analysis_list{anListIdx},'results') )
+        %search for the required data type
+        
+        dataList = analysis_list{anListIdx}.results;
+        
+        % Handle the possibility that the results is simply a flag and that the
+        % results are stored to disk, e.g. if the analysis was run using
+        % ensemble_jobman_parallel.
+        if isnumeric(dataList) && dataList == 1 && ...
+            isfield(analysis_list{anListIdx},'path') && ~isempty(analysis_list{anListIdx}.path)
+          tmp = load(analysis_list{anListIdx}.path);
+          dataList = tmp.job_data.results;
+        end
+        
+        %see if 'vars' was specified, in which case we need to
+        %search for the variable to return
+        if(isfield(requiredData,'vars') && ~isempty(requiredData.vars))
+          
+          
+          %find the index of the result we need
+          dataListIdx = strmatch(requiredData.vars,dataList.vars);
+          
+          %if result data struct was found then assign to indata
+          %otherwise, throw an error indicating the data is not there.
+          if(~isempty(dataListIdx))
+            indata{requiredIdx} = dataList.data{dataListIdx};
+            
+          else
+            error([sprintf('Analysis %s specifies a non-existent required',analysis_name)...
+              sprintf(' datatype %s for analysis %s',requiredData.vars,analysis_list{anListIdx}.name)]);
+          end
+          
+        else
+          %if the 'vars' field of the required data list was not
+          %specified, then return the whole results data structure
+          indata{requiredIdx} = analysis_list{anListIdx}.results;
+        end
+        
+        
+      else
+        
+        %results field wasn't populated so required data wasn't available
+        error(sprintf('Required data for analysis ''%s'' was not available.',analysis_name));
+        
+      end
+      
+    end % for requiredIdx
+    
   end %if isfield(analysis_list{ia},'requires')
   
   %if there was only one required data struct, then don't pass this
@@ -159,21 +159,21 @@ for ia = idxs
     analysisParams = [];
   end
   
-  result = fh(indata,analysisParams);      
+  result = fh(indata,analysisParams);
   
   analysis_list{ia}.results = result;
-	
-	% Check to see if the name of the result is empty, and if so, populate it
-	% with the name of this analysis
+  
+  % Check to see if the name of the result is empty, and if so, populate it
+  % with the name of this analysis
   numResults = length(result);
   for ires = 1:numResults
-		if isstruct(result(ires)) 
+    if isstruct(result(ires))
       if ~isfield(result(ires),'name') || isempty(result(ires).name)
         analysis_list{ia}.results(ires).name = analysis_name;
       else
         continue
       end
-		elseif ~isfield(analysis_list{ia}.results{ires},'name') || isempty(analysis_list{ia}.results{ires}.name)
+    elseif ~isfield(analysis_list{ia}.results{ires},'name') || isempty(analysis_list{ia}.results{ires}.name)
       analysis_list{ia}.results{ires}.name = analysis_name;
     end
   end
@@ -181,7 +181,7 @@ for ia = idxs
   %    err = lasterror;
   %    fprintf('Failed on analysis %d ...\n%s\nReturning completed analyses ...\n', ia, err.message);
   %    return
-     
+  
 end % for ia - analysis list
 
 if conn_local
