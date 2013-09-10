a1cd9018c86160697070327d989527f7f02c7f17
pjanata <pjanata@ucdavis.edu>
2013-09-10 17:29:15 +0000

    ensemble stim export enhancement
    git-svn-id: https://atonal.ucdavis.edu/repos/public/matlab@704 43baf2fa-511e-4498-8ce5-83910217ac2c

--- database/ensemble_export_stims.m
+++ database/ensemble_export_stims.m
@@ -1,4 +1,4 @@
-function ensemble_export_stims(data_st,params)
+function stim_st = ensemble_export_stims(data_st,params)
 % Copies stimuli that are registered in the Ensemble database to a folder
 %
 % Stimuli to be exported can be specified either in stimulus_id variable in
@@ -7,6 +7,7 @@ function ensemble_export_stims(data_st,params)
 %
 
 % 23Feb2013 Petr Janata
+% 10Sep2013 PJ - adapted to handle multiple experiment names simultaneously
 
 % Establish a connection to the database
 params.mysql.conn_id = mysql_make_conn(mysql_login(params));
@@ -23,20 +24,29 @@ else
   if ~any(expNameMask)
     error('Cannot locate experiment name in params structure');
   else
-    experiment_title = params.ensemble.(srchFlds{find(expNameMask,1,'first')});
+    experiment_names = params.ensemble.(srchFlds{find(expNameMask,1,'first')});
   end
 
-  % Get the experiment_id
-  mysql_str = sprintf(['SELECT experiment_id, response_table FROM experiment WHERE ' ...
-    'experiment_title = "%s";'], experiment_title);
-  [expID, respTable] = mysql(conn_id, mysql_str);
+  if ~iscell(experiment_names)
+    experiment_names = {experiment_names};
+  end
   
-  % Retrieve unique stims associated with this response table and this
-  % experiment I
-  mysql_str = sprintf(['SELECT DISTINCT stimulus_id FROM %s ' ...
-    'WHERE experiment_id = %d AND stimulus_id IS NOT NULL;'], respTable{1}, expID);
-  stimids = mysql(conn_id, mysql_str);
-  
+  nexp = length(experiment_names);
+  stimids = [];
+  for iexp = 1:nexp
+    experiment_title = experiment_names{iexp};
+    
+    % Get the experiment_id
+    mysql_str = sprintf(['SELECT experiment_id, response_table FROM experiment WHERE ' ...
+      'experiment_title = "%s";'], experiment_title);
+    [expID, respTable] = mysql(conn_id, mysql_str);
+    
+    % Retrieve unique stims associated with this response table and this
+    % experiment I
+    mysql_str = sprintf(['SELECT DISTINCT stimulus_id FROM %s ' ...
+      'WHERE experiment_id = %d AND stimulus_id IS NOT NULL;'], respTable{1}, expID);
+    stimids = union(stimids, mysql(conn_id, mysql_str));
+  end % for iexp
 end
 
 % Now retrieve stimulus location info
@@ -50,6 +60,7 @@ params.funcParams.stimroot = params.ensemble.stimroot;
 params.funcParams.outpath = params.paths.stimpath;
 
 stim_st = stim_st.data{scols.stimulus_metadata};
+fprintf('Processing stimuli with function: %s\n', params.funcName);
 ensemble_processStims(stim_st,params);
 
 return
